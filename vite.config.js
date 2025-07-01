05abee5eab56d65dc91fe34022642db997afcbf9
Marina Kuznetsova <marinakuznetsova420@gmail.com>
2025-07-01 16:55:52 +0300

    configuration fixes had been added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools' // пока не включаем
 
 export default defineConfig({
+  base: '/messages/',
   plugins: [
     vue(),
-    // vueDevTools(),
   ],
   resolve: {
     alias: {
